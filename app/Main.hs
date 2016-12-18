a5deaf443ebed66e12a586f585ee5f160e00df9a
eelcoh <ehoekema@gmail.com>
2016-12-18 21:32:26 +0100

    after removal of Utils.Bytes and Utils.Xor

--- app/Main.hs
+++ app/Main.hs
@@ -27,11 +27,13 @@ main = do
 
 s1ch1 :: IO ()
 s1ch1 =
-  print $ C1.challenge hexString
+  do
+    putStrLn "Set 1, challenge 1"
+    putStrLn $ C1.challenge hexString
 
-  where
-    hexString =
-      "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    where
+      hexString =
+        "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
 
 s1ch3 :: IO ()
@@ -44,18 +46,25 @@ s1ch3 =
 
     showRes c =
       do
+        putStrLn "Set 1, challenge 3"
         putStrLn $ show c
 
     resultStr =
       "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
       --"1c0111001f010100061a024b53535009181c"
 
-s1ch3a :: IO ()
-s1ch3a =
-  Frequency.frequency "Cooking MC's like a pound of bacon"
-  |> Frequency.fractionFrequency
-  |> show
-  |> print
+s1ch3 :: IO ()
+s1ch3 =
+  do
+    putStrLn "Set 1, challenge 3"
+    putStrLn doChallenge
+
+    where
+      doChallenge =
+        Frequency.frequency "Cooking MC's like a pound of bacon"
+        |> Frequency.fractionFrequency
+        |> show
+        |> print
 
 s1ch4 :: IO ()
 s1ch4 =
@@ -66,18 +75,21 @@ s1ch4 =
 
     showRes c =
       do
+        putStrLn "Set 1, challenge 5"
         putStrLn $ show c
 
 s1ch5 :: IO ()
 s1ch5 =
   do
-    putStrLn $ C5.challenge stringToCipher "ICE" 
+    putStrLn "Set 1, challenge 5"
+    putStrLn $ C5.challenge stringToCipher "ICE"
     where
       stringToCipher = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 
 s1ch6 ::IO ()
 s1ch6 =
   do
+    putStrLn "Set 1, challenge 6"
     fileContents <- B.readFile "./static/6.txt"
     mapM_ (putStrLn . showRes) $ C6.challenge fileContents
 
@@ -88,6 +100,7 @@ s1ch6 =
 s1ch6b :: IO ()
 s1ch6b =
   do
+    putStrLn "Set 1, challenge 6, using the input from challenge 5"
     mapM_ (putStrLn . showRes) $ C6.challenge str2decrypt
 
     where
